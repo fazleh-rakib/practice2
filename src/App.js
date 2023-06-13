e6f2828d710606cfb6a45ce586c3c21ffa458f25
Faz Rak <77926707+fazleh-rakib@users.noreply.github.com>
2023-06-14 00:13:05 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h2 className="text-3xl fnt bold">This is Header </h2>
+      <p>This is a paragraph</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Link from "../Link/Link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+  const routes = [
+    { id: 1, name: "Home", path: "/home" },
+    { id: 2, name: "Product", path: "/product" },
+    { id: 3, name: "Contact", path: "/contact" },
+    { id: 3, name: "About", path: "/about" },
+  ];
+  return (
+    <nav className="bg-purple-200 w-full">
+      <div onClick={() => setOpen(!open)} className="h-6 w-6 md:hidden">
+        {open ? <XMarkIcon /> : <Bars3Icon />}
+      </div>
+      <ul
+        className={`bg-purple-200  md:flex justify-center w-full font-medium absolute duration-500 ease-in md:static ${
+          open ? "top-6" : "top-[-1222px]"
+        }`}
+      >
+        {routes.map((route) => (
+          <Link key={route.id} route={route} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
